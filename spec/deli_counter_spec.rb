f87c73312942ef03cee60753d4ff56df5c8bfec6
Carlos Barreiro <bcarlosro@gmail.com>
2019-12-18 00:37:57 +0000

    Done.

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -74,3 +74,23 @@ describe 'Deli Counter' do
   end
 
 end
+
+katz_deli = []
+
+
+def line(array)
+  if array.length >0
+    new_array = []
+    counter = 1 
+    array.each do |name|
+      new_array.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+
+def take_a_number(line, new_person)
