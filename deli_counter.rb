82055561218532031aba43fffd77b7458e91a06a
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-19 22:51:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+#require "pry"
+
+def line(customers)
+#binding.pry
+  if customers.length ==  0  
+    puts "The line is currently empty."
+  elsif customers.length > 0 
+    how_busy_are_we = "The line is currently:"
+    customers.each_with_index do |person, place_in_line|
+      how_busy_are_we << " #{place_in_line+1}. #{person}"
+      end
+      puts how_busy_are_we
+  end
+end
+
+
+def take_a_number(line, name)
+   line << "#{name}"
+  if line.length == 0 
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+    elsif line.length > 0  
+      puts "Welcome, #{name}. You are number #{line.length} in line."
+      end
+end 
+
+
+def now_serving(line)
+  if line.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    now_serving = "Currently serving #{line.first}."
+    line.shift
+    puts now_serving
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
